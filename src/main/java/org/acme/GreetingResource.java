35567c20c3a94f46e425dd2f942a0a5ec917cfd3
lunnos <luan967@hotmail.com>
2023-10-24 09:00:05 -0300

    Adição da tarefa no github

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @ConfigProperty(name = "Ifrs.pw2")
+    String message;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return message;
+    }
+}
+
